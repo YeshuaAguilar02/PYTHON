fde46527831657f1218b4cc52c159188e7a07e74
YeshuaAguilar02 <elchiaguilar@gmail.com>
2015-03-02 22:30:35 -0600

    Create WSQ03.py

---
+++ WSQ03.py
@@ -0,0 +1,12 @@
+x = int( input("Put a number:"))
+y = int( input("Put another number:"))
+z = x + y
+w = x - y
+t = x * y
+u = int(x / y)
+r = x % y
+print( "The sum between", x, "and", y, "is:", z)
+print( "The difference between", x, "and", y, "is:", w)
+print( "The product between", x, "and", y, "is:", t)
+print( "The division between", x, "and", y, "is:", u)
+print( "The remainder between", x, "and", y, "is:", r)
